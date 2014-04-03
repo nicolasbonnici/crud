f5f98f8cb4c979da5e25b89b205cfa2fe26979fa
Nicolas Bonnici <nicolasbonnici@gmail.com>
2014-04-04 01:37:06 +0200

    "Auto generated commit message for crud bundle by sociableCore framework console app, see diff for more infos"

--- Controllers/CrudController.php
+++ Controllers/CrudController.php
@@ -5,12 +5,12 @@ namespace bundles\crud\Controllers;
  * Crud HomeController
  *
  * @todo Tout est réunis dans le Core pour faire un scaffold des forms
- *      
+ *
  *       Check user's permissions with the \Libray\Core\ACL component layer first then the CRUD model methods and finaly
  *       the \Libray\Core\Entity component to ensure data integrity on write access and perform the database request
- *      
+ *
  * @author Nicolas Bonnici
- *        
+ *
  */
 class CrudController extends \Library\Core\Auth
 {
@@ -48,28 +48,28 @@ class CrudController extends \Library\Core\Auth
     public function __preDispatch()
     {
         $this->_view['iStatus'] = self::XHR_STATUS_ERROR;
-        
+
         if (count($this->aEntitiesScope) > 0 && ! in_array($this->_params['entity'], $this->aEntitiesScope)) {
-            throw new CrudControllerException('Entity restricted in CrudController scope', \bundles\crud\Models\Crud::ERROR_FORBIDDEN_BY_ACL);
+            throw new CrudControllerException('Entity restricted in CrudController scope', \Library\Core\Crud::ERROR_FORBIDDEN_BY_ACL);
         }
-        
+
         if ($this->oUser->getId() !== intval($this->_session['iduser'])) {
-            throw new CrudControllerException('User session is invalid', \bundles\crud\Models\Crud::ERROR_USER_INVALID);
+            throw new CrudControllerException('User session is invalid', \Library\Core\Crud::ERROR_USER_INVALID);
         }
-        
+
         // Check user permissions on entity then entity itself
         if (isset($this->_params['entity']) && ($sEntityName = $this->_params['entity']) && strlen($sEntityName) > 0 && ($sAction = strtolower(substr($this->_controller, 0, (strlen($this->_controller) - strlen('controller'))))) && in_array($sAction, $this->aActionsScope) && ($sCheckMethodName = 'has' . $sAction . 'Access') && method_exists($this, $sCheckMethodName) && $this->{$sCheckMethodName}(strtolower($sEntityName))) {
-            
+
             try {
                 $iPrimaryKey = ((isset($this->_params['pk']) && intval($this->_params['pk']) > 0) ? intval($this->_params['pk']) : 0);
-                
+
                 // Check Entity instance with Crud model constructor
                 $this->oCrudModel = new \bundles\crud\Models\Crud(ucfirst($sEntityName), $iPrimaryKey, $this->oUser);
             } catch (\bundles\crud\Models\CrudModelException $oException) {
-                throw new CrudControllerException('Invalid Entity requested!', \bundles\crud\Models\Crud::ERROR_ENTITY_NOT_LOADABLE);
+                throw new CrudControllerException('Invalid Entity requested!', \Library\Core\Crud::ERROR_ENTITY_NOT_LOADABLE);
             }
         } else {
-            throw new CrudControllerException('Error forbidden by ACL or unauthorized action: ' . $this->_controller, \bundles\crud\Models\Crud::ERROR_FORBIDDEN_BY_ACL);
+            throw new CrudControllerException('Error forbidden by ACL or unauthorized action: ' . $this->_controller, \Library\Core\Crud::ERROR_FORBIDDEN_BY_ACL);
         }
     }
 
--- Controllers/ReadController.php
+++ Controllers/ReadController.php
@@ -5,12 +5,12 @@ namespace bundles\crud\Controllers;
  * Crud ReadController
  *
  * @todo Tout est réunis dans le Core pour faire un scaffold des forms
- *      
+ *
  *       Check user's permissions with the \Libray\Core\ACL component layer first then the CRUD model methods and finaly
  *       the \Libray\Core\Entity component to ensure data integrity on write access and perform the database request
- *      
+ *
  * @author Nicolas Bonnici
- *        
+ *
  */
 class ReadController extends CrudController
 {
@@ -23,7 +23,7 @@ class ReadController extends CrudController
     /**
      * Read an entity then pass it to a given view
      *
-     * @param string $sViewTpl            
+     * @param string $sViewTpl
      */
     public function indexAction($sViewTpl = 'read/read.tpl')
     {
@@ -32,7 +32,9 @@ class ReadController extends CrudController
             if (isset($this->_params['view']) && strlen(isset($this->_params['view'])) > 0) {
                 $sViewTpl = $this->_params['view'];
             }
-            
+
+            $oEntity = $this->oCrudModel->read();
+            $oEntity->pk = $oEntity->getId();
             $this->_view['oEntity'] = $this->oCrudModel->read();
             $this->_view['iStatus'] = self::XHR_STATUS_OK;
         } catch (\bundles\crud\Models\CrudModelException $oException) {
