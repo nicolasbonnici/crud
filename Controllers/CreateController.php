93f8b53f3b552be09225f1c26aacb372ac4db712
Nicolas Bonnici <nicolasbonnici@gmail.com>
2014-05-26 01:39:43 +0200

    "Auto generated commit message for crud bundle by sociableCore framework console app, see diff for more infos"

--- Controllers/CreateController.php
+++ Controllers/CreateController.php
@@ -5,12 +5,12 @@ namespace bundles\crud\Controllers;
  * Crud CreateController
  *
  * @todo Tout est réunis dans le Core pour faire un scaffold des forms
- *      
+ *
  *       Check user's permissions with the \Libray\Core\ACL component layer first then the CRUD model methods and finaly
  *       the \Libray\Core\Entity component to ensure data integrity on write access and perform the database request
- *      
+ *
  * @author Nicolas Bonnici
- *        
+ *
  */
 class CreateController extends CrudController
 {
@@ -23,21 +23,21 @@ class CreateController extends CrudController
     /**
      * Create an \app\Entities entity object then pass it to a given view
      *
-     * @param string $sViewTpl            
+     * @param string $sViewTpl
      */
-    public function indexAction($sViewTpl = 'create/create.tpl')
+    public function indexAction($sViewTpl = 'crud/create.tpl')
     {
         assert('$this->oCrudModel instanceof \bundles\crud\Models\Crud');
         try {
             // Toutes les données du formulaire en JSON
             if (isset($this->aParams['parameters'])) {
-                $aParameters = json_decode($this->aParams['parameters'], true);
+                $aParameters = $this->aParams['parameters'];
             }
-            
+
             if (isset($this->aParams['view']) && strlen(isset($this->aParams['view'])) > 0) {
                 $sViewTpl = $this->aParams['view'];
             }
-            
+
             if ($this->oCrudModel->create($aParameters)) {
                 $this->aView['bCreateEntity'] = true;
                 $this->aView['iStatus'] = self::XHR_STATUS_OK;
@@ -50,7 +50,7 @@ class CreateController extends CrudController
             $this->aView['error_message'] = $oException->getMessage();
             $this->aView['error_code'] = $oException->getCode();
         }
-        
+
         $this->oView->render($this->aView, $sViewTpl, $this->aView['iStatus'], false, true);
     }
 }
